edb174718736bd9127950525f13699aca593abfc
Maykon Costa <jose.brito@maisunifacisa.com.br>
2024-04-29 11:58:55 -0300

    feat - adicionando cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { checkDatabase } from './config/config.js';
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173'  // Substitua 'http://example.com' pelo URL do seu front-end
+}));
 
 import clientRoutes from './route/clientRoutes.js';
 
