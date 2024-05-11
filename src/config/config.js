c5176594d1528c21bf7dc1e893e73bcdb727445f
Maykon Costa <jose.brito@maisunifacisa.com.br>
2024-05-11 10:18:08 -0300

    feat - add banco de dev

--- src/config/config.js
+++ src/config/config.js
@@ -1,61 +1,72 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 dotenv.config();
 
-
 // Configuração para ambiente atual, assumindo NODE_ENV está definido, ou default para 'development'
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
 const config = {
   development: {
-    username: 'postgres',
-    password: '1234',
-    database: 'vivaemmovimento',
-    host: 'localhost',
-    dialect: 'postgres',
+    username: "postgres",
+    password: "1234",
+    database: "vivaemmovimento",
+    host: "paixaodecristorsa.site",
+    dialect: "postgres",
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'postgres',
+    dialect: "postgres",
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'postgres',
-  }
+    dialect: "postgres",
+  },
 }[env];
 
 // Criando uma instância do Sequelize com as configurações específicas do ambiente
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
 
-export const checkDatabase = async() => {
+export const checkDatabase = async () => {
   console.log(config);
   try {
-      await sequelize.authenticate();
-      console.log('Conexão com o banco de dados estabelecida com sucesso.');
+    await sequelize.authenticate();
+    console.log("Conexão com o banco de dados estabelecida com sucesso.");
   } catch (error) {
-      if (error.name === 'SequelizeConnectionError') {
-          console.log('Banco de dados não encontrado, criando novo banco de dados...');
-          const adminConnection = new Sequelize('postgres', config.username, config.password, {
-              host: config.host,
-              dialect: config.dialect,
-              port: config.port,
-              logging: false
-          });
-          await adminConnection.query(`CREATE DATABASE "${config.database}"`);
-          console.log(`Banco de dados ${config.database} criado com sucesso.`);
-          await adminConnection.close();
-          // Recuperar após criação
-          await sequelize.authenticate();
-      } else {
-          console.error('Erro ao conectar ao banco de dados:', error);
-          throw error;
-      }
+    if (error.name === "SequelizeConnectionError") {
+      console.log(
+        "Banco de dados não encontrado, criando novo banco de dados..."
+      );
+      const adminConnection = new Sequelize(
+        "postgres",
+        config.username,
+        config.password,
+        {
+          host: config.host,
+          dialect: config.dialect,
+          port: config.port,
+          logging: false,
+        }
+      );
+      await adminConnection.query(`CREATE DATABASE "${config.database}"`);
+      console.log(`Banco de dados ${config.database} criado com sucesso.`);
+      await adminConnection.close();
+      // Recuperar após criação
+      await sequelize.authenticate();
+    } else {
+      console.error("Erro ao conectar ao banco de dados:", error);
+      throw error;
+    }
   }
-}
+};
 
 export default sequelize;
