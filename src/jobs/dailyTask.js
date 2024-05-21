c37154c3989c67baa1e0889ba8bd2db22e7722f3
Maykon Costa <jose.brito@maisunifacisa.com.br>
2024-05-21 00:51:56 -0300

    comentando mensaagens

--- src/jobs/dailyTask.js
+++ src/jobs/dailyTask.js
@@ -5,7 +5,7 @@ import { sendDailyMessages } from "../service/utilsService.js";
 cron.schedule("0 * * * * *", async () => {
   console.log("Iniciando tarefa diária às 8h");
   try {
-    sendDailyMessages();
+    // sendDailyMessages();
     console.log("Mensagens enviadas com sucesso");
   } catch (error) {
     console.error("Erro ao enviar mensagens:", error);
