6bfe7c2f8aaba3db1133defc5513361cf938d8d8
Maykon Costa <jose.brito@maisunifacisa.com.br>
2024-05-21 20:50:39 -0300

    fix - convert DTO

--- src/model/dto/monthlyReportUserDTO.js
+++ src/model/dto/monthlyReportUserDTO.js
@@ -18,10 +18,10 @@ class monthlyReportUserDTO {
       this.faturamentoAnterior = data[1].amount;
       this.nomeMesAnterior = data[1].monthName;
     } else {
-      this.entradasAtual = data.newClients;
-      this.saidasAtual = data.clientsLeft;
-      this.faturamentoAtual = data.amount;
-      this.nomeMesAtual = data.monthName;
+      this.entradasAtual = data[0].newClients;
+      this.saidasAtual = data[0].clientsLeft;
+      this.faturamentoAtual = data[0].amount;
+      this.nomeMesAtual = data[0].monthName;
     }
   }
 }
